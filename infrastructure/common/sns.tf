6e1d2703e6ce8c588444a0492bc9b2ccd8f171b0
Vadym Melnyk <melnykv123@gmail.com>
2026-09-09 21:32:11 +0300

    The last fix of sns and vars

--- infrastructure/common/sns.tf
+++ infrastructure/common/sns.tf
@@ -12,32 +12,37 @@ resource "aws_sns_topic_subscription" "autoscaling_email_subscription" {
   endpoint = var.notification_email
 }
 
+data "aws_iam_policy_document" "autoscaling_notifications_policy" {
+  statement {
+    sid = "AllowEventBridgeToPublish"
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    actions = [
+      "SNS:Publish"
+    ]
+
+    resources = [
+      aws_sns_topic.autoscaling_notifications.arn
+    ]
+
+    condition {
+      test = "ArnEquals"
+      variable = "aws:SourceArn"
+
+      values = [
+        aws_cloudwatch_event_rule.autoscaling_events.arn
+      ]
+    }
+
+  }
+}
+
 resource "aws_sns_topic_policy" "autoscaling_notifications_policy" {
   arn = aws_sns_topic.autoscaling_notifications.arn
-  policy = jsonencode({
-    Version = "2026-09-09"
-
-    Statement = [
-      {
-        Sid = "AllowCloudWatchEventsToPublish"
-        Effect = "Allow"
-
-        Principal = {
-          Service = "events.amazonaws.com"
-        }
-
-        Action = [
-          "SNS:Publish"
-        ]
-
-        Resource = aws_sns_topic.autoscaling_notifications.arn
-
-        Condition = {
-          ArnEquals = {
-            "aws:SourceArn" = aws_cloudwatch_event_rule.autoscaling_events.arn
-          }
-        }
-      }
-    ]
-  })
+  policy = data.aws_iam_policy_document.autoscaling_notifications_policy.json
 }
--- infrastructure/env/dev.tfvars
+++ infrastructure/env/dev.tfvars
@@ -5,7 +5,6 @@ public_subnet_cidrs = ["10.20.1.0/24", "10.20.2.0/24"]
 private_subnet_cidrs = ["10.20.3.0/24", "10.20.4.0/24"]
 availability_zones = ["eu-central-1a", "eu-central-1c"]
 
-ssh_key = "dev-key"
 instance_type        = "t3.micro"
 ami_id               = "ami-0303e2e4a29f041a3"
 asg_min_size         = 1
