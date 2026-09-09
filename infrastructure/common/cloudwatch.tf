6f36f437a6de202118359bb7643da647d5f01f47
Vadym Melnyk <melnykv123@gmail.com>
2026-09-09 20:22:30 +0300

    Fixed CloudWatch configuration

--- infrastructure/common/cloudwatch.tf
+++ infrastructure/common/cloudwatch.tf
@@ -18,5 +18,9 @@ resource "aws_cloudwatch_event_rule" "autoscaling_events" {
       ]
     }
   })
+}
 
+resource "aws_cloudwatch_event_target" "autoscaling_sns_target" {
+  rule = aws_cloudwatch_event_rule.autoscaling_events.name
+  arn = aws_sns_topic.autoscaling_notifications.arn
 }
